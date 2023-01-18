c67619a1d7305abf696058153ffcf30081e9ec9d
ys494 <ys494@cornell.edu>
2023-01-18 14:05:17 -0500

    Made a scatterplot with best fit line

--- code/un_data_analysis.R
+++ code/un_data_analysis.R
@@ -63,16 +63,43 @@ gapminder_data_2007 <- read_csv("data/gapminder_data.csv") %>%
         filter(year == 2007) %>%
         select(country, pop, lifeExp, gdpPercap)
 
+# make joined data
 joined_co2_pop <- inner_join(co2_emissions, gapminder_data_2007)
 
+# test anti_join
 anti_join(gapminder_data_2007, co2_emissions, by = "country")
-
+# test full_join
 full_join(co2_emissions, gapminder_data_2007) %>%
         View()
 
 # writing a csv
 write_csv(joined_co2_pop, file = "data/joined_co2_pop.csv")
 
+# read back in csv file
+joined_co2_pop <- read_csv("data/joined_co2_pop.csv")
+
+# ggplot histogram for both gdpPercap and lifeExp, separately, to explore those variables' distributions
+ggplot(data = joined_co2_pop, aes(x = gdpPercap)) +
+        labs(x = "GDP Per Capita", y = "Count") +
+        geom_histogram() +
+        theme_bw()
+
+ggplot(data = joined_co2_pop, aes(x = lifeExp)) +
+        labs(x = "GDP Per Capita", y = "Count") +
+        geom_histogram() +
+        theme_bw()
+
+# make scatterplot with best fit line
+gdp_co2_plot <- ggplot(data = joined_co2_pop, aes(x = gdpPercap, y = per_capita_emissions)) +
+        geom_point() +
+        geom_smooth(method = "lm", se = FALSE) +
+        labs(x = "GDP Per Capita", y = "CO2 Emissions Per Capita (metric tons)",
+             title = "Comparing Per Capita CO2 Emissions and GDP") +
+        theme_classic() +
+        ggpubr::stat_regline_equation(aes(label = after_stat(rr.label)))
+ggsave(gdp_co2_plot, filename = "figures/gdp_co2_plot.png", 
+       height = 4, width = 6, units = "in",
+       dpi = 300)
 
 
 
